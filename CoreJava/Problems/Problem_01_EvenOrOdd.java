f3ebe80624ffd30839c50c1b6cbde15444a8acb1
VishalWaje <vishalwaje09@gmail.com>
2024-12-11 16:51:08 +0530

    Initial commit

---
+++ CoreJava/Problems/Problem_01_EvenOrOdd.java
@@ -0,0 +1,24 @@
+// Problem Statement:
+// You need to write a program that reads an integer from the user and 
+// determines whether the number is even or odd.
+
+import java.util.Scanner;
+
+public class Problem_01_EvenOrOdd {
+    public static void main(String[] args) {
+
+        // Created a scanner object to read input
+        Scanner sc = new Scanner(System.in);
+
+        // Asked user to enter a number
+        System.out.println("Enter a number");
+        int number = sc.nextInt();
+
+        // Check the number is even or odd.
+        if (number % 2 == 0) {
+            System.out.println("The given number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+    }
+}
